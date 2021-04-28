974ae36488a4277da0b96240e09c6b16fb70cd4e
Marlene Vasconcelos Moraes de Oliveira <marlenevmoraes@outlook.com>
2021-04-28 02:31:38 -0300

    aula 013

---
+++ JavaScript/aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c=1
+
+do {  
+    console.log("Tudo bem?")
+} while (c<=6)
+//teste no final do bloco
+
+var c=1
+while (c<=6){
+    console.log("Tudo bem?")
+}
+//teste no início do bloco
+
+
+/*
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+*/
