a87f4f5ae0cc0c8fa263897875069d137f45cb3e
Marlene Vasconcelos Moraes de Oliveira <marlenevmoraes@outlook.com>
2021-04-28 10:58:10 -0300

    modulo e

---
+++ JavaScript/aula14/ambiente.js
@@ -0,0 +1,15 @@
+var c=1
+
+console.log("Vai começar a repetição While!")
+while (c<=5){
+    console.log(c)
+    c++
+}
+console.log("Fim do While")
+
+//teste no início do bloco
+console.log("Vai começar a repetição For!")
+for(var c=1;c<=5;c++){
+    console.log(c)
+}
+console.log("Fim do For")
