eed28f62f9e4bbba538e46bc10e805a76e72f9d7
Marlene Moraes <marlenevmoraes@outlook.com>
2021-06-24 10:27:25 -0300

    update pages

--- HTML5/projeto-glass/javascript/script.js
+++ HTML5/projeto-glass/javascript/script.js
@@ -1,3 +1,10 @@
 function mudaFoto (foto) {
     document.getElementById ("photoMenu").src = foto
 }
+
+function calculoTotal() {
+    let quantity = document.querySelector ('#quantity').value
+    total = quantity*1500
+
+    document.querySelector ('#price').value = total
+}
